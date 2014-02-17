1305ae1aee0e5f576c782f23813fd5374afe0731
Chris Keller <christopherlawrencekeller@gmail.com>
2014-02-17 13:22:57 -0800

    changes csv file open method, gives function a unique name and removes if name == main for step 4

--- project1/step_4.py
+++ project1/step_4.py
@@ -1,46 +1,42 @@
 # import modules
-import csv, logging
-
-logging.basicConfig(format='\033[1;36m%(levelname)s:\033[0;37m %(message)s', level=logging.DEBUG)
+import csv
 
 # write your function passing in the file name
 def open_csv_file(file_name):
 
-    # use the csv module to open the csv file
-    with open(file_name, 'rb') as csv_file:
+    # open the csv
+    csv_file = open(file_name, 'rb')
 
-        # create the object that represents the data in the csv file
-        csv_data = csv.reader(csv_file)
+    # create the object that represents the data in the csv file
+    csv_data = csv.reader(csv_file)
 
-        # create a variable to represent the header row
-        header_row = csv_data.next()
-        logging.debug(header_row)
+    # create a variable to represent the header row
+    header_row = csv_data.next()
+    print header_row
 
-        # create a variable to represent the length of the header row
-        header_length = len(header_row)
-        logging.debug(header_length)
+    # create a variable to represent the length of the header row
+    header_length = len(header_row)
+    print header_length
 
-        # create a variable to represent the datatype for the header row
-        header_type = type(header_row)
-        logging.debug(header_type)
+    # create a variable to represent the datatype for the header row
+    header_type = type(header_row)
+    print header_type
 
-        # loop through each item in the header_row
-        for column_name in header_row:
+    # loop through each item in the header_row
+    for column_name in header_row:
 
-            # log its contents
-            logging.debug(column_name)
+        # log its contents
+        print column_name
 
-            # log its length
-            column_name_length = len(column_name)
-            logging.debug(column_name_length)
+        # log its length
+        column_name_length = len(column_name)
+        print column_name_length
 
-            # log its type
-            column_name_type = type(column_name)
-            logging.debug(column_name_type)
+        # log its type
+        column_name_type = type(column_name)
+        print column_name_type
 
     # close the csv file when we're done
     csv_file.close()
 
-# run the function when you run the script in the terminal
-if __name__ == '__main__':
-    open_csv_file('fdic_failed_bank_list.csv')
+open_csv_file('fdic_failed_bank_list.csv')
