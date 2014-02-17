73c60613ea4768c7c787824a91ae01b8ea6099a6
Chris Keller <christopherlawrencekeller@gmail.com>
2014-02-17 13:19:27 -0800

    changes csv file open method, gives function a unique name and removes if name == main

--- project1/step_3.py
+++ project1/step_3.py
@@ -2,29 +2,27 @@
 import csv
 
 # write your function passing in the file name
-def open_csv_file(file_name):
+def output_rows_from(file_name):
 
-    # use the csv module to open the csv file
-    with open(file_name, 'rb') as csv_file:
+    # open the csv
+    csv_file = open(file_name, 'rb')
 
-        # create the object that represents the data in the csv file
-        csv_data = csv.reader(csv_file)
+    # create the object that represents the data in the csv file
+    csv_data = csv.reader(csv_file)
 
-        # loop through each row in the object
-        for row in csv_data:
+    # loop through each row in the object
+    for row in csv_data:
 
-            # output the type
-            print type(row)
+        # output the type
+        print type(row)
 
-            # output the length of the row
-            print len(row)
+        # output the length of the row
+        print len(row)
 
-            # output the contents
-            print row
+        # output the contents
+        print row
 
     # close the csv file when we're done
     csv_file.close()
 
-# run the function when you run the script in the terminal
-if __name__ == '__main__':
-    open_csv_file('fdic_failed_bank_list.csv')
+output_rows_from('fdic_failed_bank_list.csv')
