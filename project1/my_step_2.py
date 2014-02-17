00f37d7fa9eb8846fbc2cea41847d9a3db9eb8d9
Chris Keller <christopherlawrencekeller@gmail.com>
2014-02-17 15:02:40 -0800

    Adds commented-only scripts to project folder

---
+++ project1/my_step_2.py
@@ -0,0 +1,11 @@
+# import modules
+
+FILE_NAME = 'fdic_failed_bank_list.csv'
+
+# write a function to count the number of rows in a csv file
+
+    # create a variable to iterate each row and count the number of lines
+
+    # output the number of lines
+
+# run the function when you run the script in the terminal
