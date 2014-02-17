8840a130552d20e08d15c7c035ae7c2d85736020
Chris Keller <christopherlawrencekeller@gmail.com>
2014-02-17 13:15:24 -0800

    gives step 2 function a unique name

--- project1/step_2.py
+++ project1/step_2.py
@@ -2,7 +2,7 @@
 import csv
 
 # write your function passing in the file name
-def open_csv_file(file_name):
+def count_csv_rows(file_name):
 
     # create a variable to iterate each row and count the number of lines
     number_of_rows = sum(1 for line in open(file_name))
@@ -11,4 +11,4 @@ def open_csv_file(file_name):
     print number_of_rows
 
 # run the function when you run the script in the terminal
-open_csv_file('fdic_failed_bank_list.csv')
+count_csv_rows('fdic_failed_bank_list.csv')
