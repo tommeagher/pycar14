50299b03682506b2a3ce86a847f6b4809f4a4508
Kevin Schaul <kevin.schaul@gmail.com>
2015-03-05 15:29:38 -0500

    Remove if __name__ == '__main__'
    Not important for beginners

--- project4/step_1_complete.py
+++ project4/step_1_complete.py
@@ -4,28 +4,24 @@ import json
 # `requests` is a module for interacting with the Internet
 import requests
 
-def main():
-    url = 'https://www.govtrack.us/api/v2/bill?congress=114&order_by=-current_status_date'
+url = 'https://www.govtrack.us/api/v2/bill?congress=114&order_by=-current_status_date'
 
-    # Read the `requests` documentation for information. I promise it
-    # isn't that scary.
-    # http://docs.python-requests.org/en/latest/user/quickstart/#json-response-content
+# Read the `requests` documentation for information. I promise it
+# isn't that scary.
+# http://docs.python-requests.org/en/latest/user/quickstart/#json-response-content
 
-    # Request the data
-    r = requests.get(url)
+# Request the data
+r = requests.get(url)
 
-    # Since we know our data will be JSON, let's automatically convert
-    # it to a Python dict.
-    data = r.json()
+# Since we know our data will be JSON, let's automatically convert
+# it to a Python dict.
+data = r.json()
 
-    # -- OR -- If the network is down, we can use a local version of this file.
-    #with open('bills.json', 'r') as f:
-    #    data = json.load(f)
+# -- OR -- If the network is down, we can use a local version of this file.
+#with open('bills.json', 'r') as f:
+#    data = json.load(f)
 
-    # `json.dumps()` is a way to print a Python dict in a more
-    # human-readable way.
-    print json.dumps(data, indent=4)
-
-if __name__ == '__main__':
-    main()
+# `json.dumps()` is a way to print a Python dict in a more
+# human-readable way.
+print json.dumps(data, indent=4)
 
